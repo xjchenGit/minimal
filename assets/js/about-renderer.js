9029a6ed2d5e7cd3b9312f31406a5788a5db0424
xjchenGit <achenxuanjun@gmail.com>
2026-05-02 22:06:45 +0800

    update satus

--- assets/js/about-renderer.js
+++ assets/js/about-renderer.js
@@ -31,6 +31,9 @@ function loadAndRenderAbout(yamlPath, containerId) {
                 html += introHtml;
             }
 
+            // Desktop-only text-style contact links (visibility controlled by CSS)
+            html += `<p class="hero-contact-text">[<a href="mailto:D12942018@ntu.edu.tw">Email</a> | <a href="https://www.linkedin.com/in/jun-ntu/" target="_blank" rel="noopener noreferrer">Linkedin</a> | <a href="https://x.com/xjchen_ntu" target="_blank" rel="noopener noreferrer">Twitter</a> | <a href="https://github.com/xjchenGit" target="_blank" rel="noopener noreferrer">Github</a> | <a href="https://scholar.google.com/citations?user=ZDVOXd4AAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Google Scholar</a>]</p>`;
+
             container.innerHTML = html;
 
             // Add justify alignment to paragraphs to match original design
